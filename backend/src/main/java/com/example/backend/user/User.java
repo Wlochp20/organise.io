b95b51980e7bdc392f09b73f003a1431416ac75d
Wlochp20 <91265023+Wlochp20@users.noreply.github.com>
2023-01-10 20:36:23 +0100

    create user package

---
+++ backend/src/main/java/com/example/backend/user/User.java
@@ -0,0 +1,64 @@
+package com.example.backend.user;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Collection;
+
+
+@Entity
+public class User implements UserDetails {
+
+    @Id
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+    private Boolean locked = false;
+    private Boolean enabled = false;
+
+    public User(String username, String email, String password, Boolean locked, Boolean enabled) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.locked = locked;
+        this.enabled = enabled;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
